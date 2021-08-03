b822de67f65a88602d892d3001e64332a2e62f52
Mei Manzo <mmanzo806@gmail.com>
2021-08-02 18:11:27 -0700

    hide secret key

--- Group_Project_Deploy/settings.py
+++ Group_Project_Deploy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+from secret_settings import *
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jj^yzp8(-#mzn%+)zf$l1q-_(i_l8s=_qd%_7&@-4g6!kdgm75'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
